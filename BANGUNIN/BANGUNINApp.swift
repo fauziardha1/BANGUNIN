b607f8e540f0bcf72d0081820022346f652320d0
fauziardha1 <fauziardasaputra@gmail.com>
2022-09-26 18:00:56 +0700

    initial repo

---
+++ BANGUNIN/BANGUNINApp.swift
@@ -0,0 +1,17 @@
+//
+//  BANGUNINApp.swift
+//  BANGUNIN
+//
+//  Created by FauziArda on 26/09/22.
+//
+
+import SwiftUI
+
+@main
+struct BANGUNINApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
