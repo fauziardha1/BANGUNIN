9a0aabcf9a3ed6f8e301d5804e930f92e3841d47
fauziardha1 <fauziardasaputra@gmail.com>
2022-09-30 16:07:04 +0700

    add features folder

---
+++ BANGUNIN/Features/MapView/ViewModel/MapModel.swift
@@ -0,0 +1,8 @@
+//
+//  MapModel.swift
+//  BANGUNIN
+//
+//  Created by FauziArda on 30/09/22.
+//
+
+import Foundation
---
+++ BANGUNIN/Features/MapView/ViewModel/MapPageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MapPageViewModel.swift
+//  BANGUNIN
+//
+//  Created by FauziArda on 30/09/22.
+//
+
+import Foundation
