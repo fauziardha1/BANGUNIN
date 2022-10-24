f6b00d564894a23037ce40d635d4ca420f4533e2
Zevira Varies Martan <zeviravm@gmail.com>
2022-10-24 13:18:03 +0700

    - Add Picker for menu

--- BANGUNIN/Features/EditAlarmView.swift
+++ BANGUNIN/Features/EditAlarmView.swift
@@ -8,20 +8,43 @@
 import Foundation
 import SwiftUI
 
+enum Radius: String, CaseIterable,Identifiable {
+    case Orange, Blue, Yellow
+    var id: Self {self}
+}
+
+enum AlertSound: String, CaseIterable,Identifiable {
+    case Radar, Rudur , Rodor
+    var id: Self {self}
+}
+
+enum Vibration: String, CaseIterable,Identifiable {
+    case None, Normal, Custom
+    var id: Self {self}
+}
+
+enum Repeat: String, CaseIterable,Identifiable {
+    case Never, Everyday, EveryMonday
+    var id: Self {self}
+}
+
 struct EditView: View {
     @State var alarmname: String = ""
-    @State var radiusColor: String = ""
-    @State var alert: String = ""
-    @State var vibrate: String = ""
+    @State var radiusColor: Radius = .Orange
+    @State var alert: AlertSound = .Radar
+    @State var vibrate: Vibration = .None
     @State var range = 0
+    
+    
     var body: some View{
+        
         VStack(alignment: .leading, spacing: 20) {
             Text("Range:")
             Picker("Range", selection: $range) {
                 Text("100").tag(0)
                 Text("200").tag(1)
                 Text("300").tag(2)
-                Text("custom").tag(3)
+                Text("Custom").tag(3)
             }
             .pickerStyle(.segmented)
             VStack(alignment: .leading) {
@@ -35,22 +58,46 @@ struct EditView: View {
                 Text("Radius color")
                     .font(.callout)
                     .bold()
-                TextField("Choose color", text: $radiusColor)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                List {
+                    Picker("Choose Color", selection: $radiusColor) {
+                        Text("Orange").tag(Radius.Orange)
+                        Text("Blue").tag(Radius.Blue)
+                        Text("Yellow").tag(Radius.Yellow)
+                    }.pickerStyle(MenuPickerStyle())
+                }.frame(maxHeight: 100)
+                
+//                TextField("Choose color", text: $radiusColor)
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
             }
             VStack(alignment: .leading) {
                 Text("Alert")
                     .font(.callout)
                     .bold()
-                TextField("Choose alert", text: $alert)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                List{
+                    Picker("Choose Alert", selection: $alert) {
+                        Text("Radar").tag(AlertSound.Radar)
+                        Text("Rodor").tag(AlertSound.Rodor)
+                        Text("Rudur").tag(AlertSound.Rudur)
+                    }.pickerStyle(MenuPickerStyle())
+                }.frame(maxHeight: 100)
+                
+//                TextField("Choose alert", text: $alert)
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
             }
             VStack(alignment: .leading) {
                 Text("Vibration")
                     .font(.callout)
                     .bold()
-                TextField("Choose vibration", text: $vibrate)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                List {
+                    Picker("Choose Vibration", selection: $vibrate) {
+                        Text("None").tag(Vibration.None)
+                        Text("Custom").tag(Vibration.Custom)
+                        Text("Normal").tag(Vibration.Normal)
+                    }.pickerStyle(MenuPickerStyle())
+                }.frame(maxHeight: 100)
+                
+//                TextField("Choose vibration", text: $vibrate)
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
             }
             Spacer()
         }.padding()
