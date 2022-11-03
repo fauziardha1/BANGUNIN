2b191855666e68b4b9a90b5450d3d55df2cc7eba
Ardhan12 <arief.ramadhan43@gmail.com>
2022-11-03 08:37:59 +0700

    create data show data and delete data

--- BANGUNIN/Features/EditAlarmView.swift
+++ BANGUNIN/Features/EditAlarmView.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftUI
 
+
 enum Radius: String, CaseIterable,Identifiable {
     case Orange, Blue, Yellow
     var id: Self {self}
@@ -29,29 +30,43 @@ enum Repeat: String, CaseIterable,Identifiable {
 }
 
 struct EditView: View {
-    @State var alarmname: String = ""
+    let coreDM: CoreDataManager
+    @State private var alarmName: String = ""
+    @State private var radiusAlarm = 0
+        // NOT A GOOD IDEA TO USE STATE TO POPULATE DATA FROM
+        // THIRD PARTY CALL
+        @State public var alarms: [Alarm] = [Alarm]()
+        //        @State private var needsRefresh: Bool = false
+    
+//    @State var alarmname: String = ""
     @State var radiusColor: Radius = .Orange
     @State var alert: AlertSound = .Radar
     @State var vibrate: Vibration = .None
-    @State var range = 0
+//    @State var range = 0
+    
+    private func collectAlarm() {
+            alarms = coreDM.getAllAlarms()
+        }
     
     
     var body: some View{
         
-        VStack(alignment: .leading, spacing: 20) {
-            Text("Range:")
-            Picker("Range", selection: $range) {
+        VStack(alignment: .center, spacing: 20) {
+            VStack(alignment: .leading) {
+                Text("Range:")
+            Picker("Range", selection: $radiusAlarm) {
                 Text("100").tag(0)
                 Text("200").tag(1)
                 Text("300").tag(2)
                 Text("Custom").tag(3)
+            }
             }
             .pickerStyle(.segmented)
             VStack(alignment: .leading) {
                 Text("Alarm name")
                     .font(.callout)
                     .bold()
-                TextField("Name the alarm", text: $alarmname)
+                TextField("Name the alarm", text: $alarmName)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
             }
             VStack(alignment: .leading) {
@@ -65,9 +80,6 @@ struct EditView: View {
                         Text("Yellow").tag(Radius.Yellow)
                     }.pickerStyle(MenuPickerStyle())
                 }.frame(maxHeight: 100)
-                
-//                TextField("Choose color", text: $radiusColor)
-//                    .textFieldStyle(RoundedBorderTextFieldStyle())
             }
             VStack(alignment: .leading) {
                 Text("Alert")
@@ -81,8 +93,6 @@ struct EditView: View {
                     }.pickerStyle(MenuPickerStyle())
                 }.frame(maxHeight: 100)
                 
-//                TextField("Choose alert", text: $alert)
-//                    .textFieldStyle(RoundedBorderTextFieldStyle())
             }
             VStack(alignment: .leading) {
                 Text("Vibration")
@@ -95,18 +105,41 @@ struct EditView: View {
                         Text("Normal").tag(Vibration.Normal)
                     }.pickerStyle(MenuPickerStyle())
                 }.frame(maxHeight: 100)
-                
-//                TextField("Choose vibration", text: $vibrate)
-//                    .textFieldStyle(RoundedBorderTextFieldStyle())
             }
+            Button("Save") {
+                coreDM.saveAlarm(name: alarmName, radius: Double(radiusAlarm))
+                collectAlarm()
+                print("oke di sep")
+            }
+            .frame(width: 100, height: 50)
+            .background(.red)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            List {
+                ForEach(alarms, id: \.self){ alarm in
+                    VStack {
+                        Text("\(alarm.radius)")
+                        Text(alarm.name ?? "")
+                    }
+                    
+                }.onDelete(perform: {indexSet in
+                    indexSet.forEach { index in
+                        let alarm = alarms[index]
+                        coreDM.deleteAlarm(alarm: alarm)
+                        collectAlarm()
+                    }
+                })
+            }
+
             Spacer()
         }.padding()
     }
 }
 
 
+
 struct editview: PreviewProvider {
     static var previews: some View {
-        EditView()
+        EditView(coreDM: CoreDataManager())
     }
 }
---
+++ BANGUNIN/datamodel.swift
@@ -0,0 +1,72 @@
+//
+//  datamodel.swift
+//  BANGUNIN
+//
+//  Created by Arief Ramadhan on 02/11/22.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    let persistentContainer: NSPersistentContainer
+    
+    init() {
+        persistentContainer = NSPersistentContainer(name: "Model")
+        persistentContainer.loadPersistentStores { (description, error) in
+            if let error = error {
+                fatalError("Core Data Store failed \(error.localizedDescription)")
+            }
+        }
+    }
+    
+            func updateAlarm() {
+    
+                do {
+                    try persistentContainer.viewContext.save()
+                } catch {
+                    persistentContainer.viewContext.rollback()
+                }
+    
+            }
+    //
+            func deleteAlarm(alarm: Alarm) {
+    
+                persistentContainer.viewContext.delete(alarm)
+    
+                do {
+                    try persistentContainer.viewContext.save()
+                } catch {
+                    persistentContainer.viewContext.rollback()
+                    print("Failed to save context \(error)")
+                }
+    
+            }
+    
+            func getAllAlarms() -> [Alarm] {
+    
+                let fetchRequest: NSFetchRequest<Alarm> = Alarm.fetchRequest()
+    
+                do {
+                    return try persistentContainer.viewContext.fetch(fetchRequest)
+                } catch {
+                    return []
+                }
+    
+            }
+    //
+    func saveAlarm(name: String, radius: Double) {
+        
+        let alarm = Alarm(context: persistentContainer.viewContext)
+        alarm.name = name
+        alarm.radius = radius
+        
+        do {
+            try persistentContainer.viewContext.save()
+        } catch {
+            print("Failed to save movie \(error)")
+        }
+        
+    }
+    
+}
