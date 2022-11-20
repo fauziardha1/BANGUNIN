71263bcd8a6b14cd833211ba90393ed146594b4b
Ardhan12 <arief.ramadhan43@gmail.com>
2022-11-20 16:37:56 +0700

    update ui

--- BANGUNIN/BANGUNINApp.swift
+++ BANGUNIN/BANGUNINApp.swift
@@ -7,40 +7,15 @@
 
 import SwiftUI
 
-enum Screen {
-    case homepage
-    case listpage
-}
-
-final class TabRouter: ObservableObject {
-    @Published var screen: Screen = .homepage
-    
-    func change(to screen: Screen) {
-        self.screen = screen
-    }
-}
-
 @main
 struct BANGUNINApp: App {
     
     @StateObject var router = TabRouter()
     
     var body: some Scene {
+//        
         WindowGroup {
-            TabView(selection: $router.screen) {
-                MapPage()
-                    .tag(Screen.homepage)
-                    .environmentObject(router)
-                    .tabItem {
-                        Label("Home", systemImage: "house")
-                    }
-                AlarmList()
-                    .tag(Screen.listpage)
-                    .environmentObject(router)
-                    .tabItem {
-                        Label("List", systemImage: "bookmark")
-                    }
-            }.accentColor(.orange)
+            customTabBar(router: TabRouter())
         }
     }
 }
